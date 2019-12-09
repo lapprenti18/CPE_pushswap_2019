fbdf4d770890fc20b5138964e7653cf5c1fbfad9
emeryck <pokspid@gmail.com>
2019-12-09 17:46:42 +0100

    fix norme

--- my_amazing_sorter.c
+++ my_amazing_sorter.c
@@ -27,7 +27,7 @@ int    *rotate_right(int *array, int size)
 {
     int i = array[size - 1];
 
-    for (; size != 0; size -= 1) 
+    for (; size != 0; size -= 1)
         array[size] = array[size - 1];
     array[0] = i;
     return (array);
@@ -73,50 +73,60 @@ int    nbr_min_position(int *array,  int size)
     return (position);
 }
 
-void    my_amazing_sorter(int *array, int size, int *array2)
+void    display(int size)
 {
-    int nbr_min;
-
-    for (int temp = 0; temp != size; temp += 1) {
-        nbr_min = nbr_min_position(array, size);
-        for (int a = 0; a != nbr_min; a += 1) {
-            rotate_left(array, size);
-            write(1, "ra ", 3);
-        }
-        write (1, "pa ", 3);
-        array2[size - 1 - temp] = array[0];
-        array[0] = 36;
-    }
-    array = array2;
+    write (1, "pb ", 3);
     for (int a = 0; a != size - 1; a += 1)
         write(1, "pa ", 3);
     write(1, "pa", 2);
-    //for (int a = 0; a < size / 2; a += 1)
-    //    array[a] = array[size - a];
-    //for (int a = 0; a != size; a += 1)
-    //    printf("array[%d] = %d\n", a, array[a]);
 }
 
-int main(int ac, char **av)
+void    my_amazing_sorter(int *array, int size, int *array2)
 {
+    int nbr_min;
+    int jsp = 0;
 
-    int *array;
-    int *arry2;
+    for (int temp = 0; temp != size - 1; temp += 1) {
+        nbr_min = nbr_min_position(array, size);
+        for (int a = 0; a != nbr_min; a += 1) {
+            if (array[0] == 36)
+                jsp += 1;
+            rotate_left(array, size);
+        }
+        for (int b = 0; b + jsp < nbr_min; b += 1)
+            write(1, "ra ", 3);
+        write (1, "pb ", 3);
+        array2[size - 1 - temp] = array[0];
+        array[0] = 36;
+        jsp = 0;
+    }
+    display(size);
+}
+
+int *array_empty(int *array, int ac, char **av)
+{
     int b = 1;
 
-    if (ac == 1)
-        return (84);
-    if (ac == 2) {
-        array = malloc(sizeof(int) * 1);
-        array[0] =  my_getnbr(av[1]);
-        return (0);
-    }
-    array = malloc(sizeof(int) * (ac - 1));
-    arry2 = malloc(sizeof(int) * (ac - 1));
     for (int a = 0; a < (ac - 1); a += 1) {
         array[a] = my_getnbr(av[b]);
         b += 1;
     }
+    return (array);
+}
+
+int main(int ac, char **av)
+{
+    int *array;
+    int *arry2;
+
+    if (ac == 1)
+        return (84);
+    if (ac == 2) {
+        return (0);
+    }
+    array = malloc(sizeof(int) * (ac - 1));
+    arry2 = malloc(sizeof(int) * (ac - 1));
+    array = array_empty(array, ac, av);
     if (array_good(array, ac - 1) == 0) {
         return (0);
     }
