9d418165b6503df2c4e538641b569e48005abfaf
emeryck <pokspid@gmail.com>
2019-12-13 15:05:09 +0100

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit fefd3e91b13de2542c9bee44f5e3f53ba0d7ebdc.

--- my.h
+++ my.h
@@ -18,12 +18,3 @@ typedef struct lulu_s
     char *popo;
     int louis;
 }lulu_t;
-
-int    *swap(int *array);
-int    *rotate_right(int *array, int size);
-int    *rotate_left(int *array, int size);
-int    array_good(int *array, int size);
-int    nbr_min_position(int *array,  int size);
-void    display(int size, lulu_t *lulu);
-void    my_amazing_sorter(int *array, int size, int *array2, lulu_t *lulu);
-int *array_empty(int *array, int ac, char **av);
